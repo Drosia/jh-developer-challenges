f415f10069c1c3f2228c32f002a5a166e7c9afc6
Drosia <lykosdim@gmail.com>
2015-10-23 16:26:09 +0100

    Lykos Dimitris

--- src/ShippingDates.php
+++ src/ShippingDates.php
@@ -2,10 +2,28 @@
 
 namespace Jh\Shipping;
 /**
- * Class ShippingDates
+ * Class ShippingDates                          
  * @package Jh\Shipping
  */
-class ShippingDates implements ShippingDatesInterface
+class ShippingDates 
 {
 
+    /**
+     * @param \DateTime $orderDate
+     * @return \DateTime 
+     */
+
+public function calculateDeliveryDate(\DateTime $orderDate){
+   
+
+        $day = $orderDate->format('w');
+        if ($day == 0 ){
+            $deliveryDate = $orderDate->modify('+1 day');
+        }
+        elseif( $day == 3 || $day == 4 || $day == 5 || $day == 6 ){
+            $deliveryDate = $orderDate->modify('+2 day');
+        }
+        $deliveryDate = $orderDate->modify('+3 day');
+        return $deliveryDate;
+    }
 }
